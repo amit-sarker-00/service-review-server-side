7016d39c400cc13bf0dcccc8ba7b55ecd56c4b23
Amit Sarker <amitsarker255@gmail.com>
2022-11-08 03:19:20 +0600

    connection to mongodb and send data to the client side

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 // const jwt=require('jsonwebtoken')
@@ -8,6 +9,29 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qpavz6c.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const photoCollection = client.db("wildLover").collection("services");
+
+    //get services
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = photoCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("WildRidder");

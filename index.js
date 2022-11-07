384d5526c6904a3d5ed9eb47cd70b063facc9ab5
Amit Sarker <amitsarker255@gmail.com>
2022-11-08 02:24:30 +0600

    server site setup done and git fatal problem solve

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+// const jwt=require('jsonwebtoken')
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("WildRidder");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
